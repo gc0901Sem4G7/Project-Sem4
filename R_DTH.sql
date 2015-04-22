5fc2d1cd3ef2c8da873bbe78aad4d54fc400ff1f
tungdeptrai <caubenuoc1995@gmail.com>
2015-04-22 21:32:25 +0700

    database chưa hoàn thiện

---
+++ R_DTH.sql
@@ -0,0 +1,25 @@
+Create Database R_DTH
+go
+use R_DTH
+
+go
+create table tbl_admin(
+	adminID int primary key,
+	adminName nvarchar(100),
+	adminPass nvarchar(100)
+)
+
+go
+create table tbl_FAQ(
+	faqID int primary key,
+	faqQuestion ntext,
+	faqAnswer ntext
+)
+
+go
+create table tbl_user(
+	userID int primary key,
+	userPass nvarchar(100),
+	cardNumber nvarchar(100)
+)
+
