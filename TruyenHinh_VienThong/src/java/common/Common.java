ed50a38c514cc132cf9fabff7c79f51322adec47
minhminh <diezero0807@gmail.com>
2015-06-23 20:54:19 +0700

    them common

---
+++ TruyenHinh_VienThong/src/java/common/Common.java
@@ -0,0 +1,8 @@
+package common;
+
+public class Common {
+	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	public static final String DB_URL = "jdbc:mysql://localhost/demo";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "";
+}
