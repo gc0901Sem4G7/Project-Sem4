5849b610085947d8c5bc122d7b27ccbf23e0d704
minhminh <diezero0807@gmail.com>
2015-09-04 23:47:57 +0700

    fix

--- TruyenHinh_VienThong/src/java/servlet/admin/AdminDichVuServlet.java
+++ TruyenHinh_VienThong/src/java/servlet/admin/AdminDichVuServlet.java
@@ -1,5 +1,6 @@
 package servlet.admin;
 
+import doituong.Dealer;
 import java.io.IOException;
 import java.util.List;
 
@@ -11,11 +12,55 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.admin.AdminDichVuModel;
 import doituong.DichVu;
+import doituong.Order;
+import javax.servlet.http.HttpSession;
+import model.OrderModel;
+import model.UserModel;
+import model.admin.AdminDealerModel;
 
 public class AdminDichVuServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String url = request.getRequestURL().toString();
+        String acction = request.getParameter("action");
+        AdminDichVuModel adminDichVuModel = new AdminDichVuModel();
+        AdminDealerModel adminDealerModel = new AdminDealerModel();
+        UserModel userModel = new UserModel();
+        HttpSession httpSession = request.getSession();
+        String admin = httpSession.getAttribute("admin") + "";
+        String mien = userModel.getMienOfDealerByUsername("dealer", admin);
+        OrderModel orderModel = new OrderModel();
+        int dealerId = 0;
+
+        Dealer dealer = adminDealerModel.getDealerByUsername(admin);
+        if (url.endsWith("/admin/do-them-moi-dich-vu")) {
+            String tenDichVu = request.getParameter("tenDichVu");
+            String giaDichVu = request.getParameter("giaDichVu");
+            String moTa = request.getParameter("moTa");
+            
+            dealerId = dealer.getId();
+            DichVu dichVu = new DichVu(tenDichVu, giaDichVu, moTa, dealerId, "waiting", mien);
+            adminDichVuModel.insert(dichVu, "dich_vu");
+        }
+        if ("denied".equals(acction)) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            adminDichVuModel.delete(id, "dich_vu");
+        } else if ("accept".equals(acction)) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            DichVu dichVuObj = adminDichVuModel.getDichVuById(id, "dich_vu");
+            dichVuObj.setTrangThai("accepted");
+            adminDichVuModel.update(dichVuObj, "dich_vu");
+        } else if ("finish".equals(acction)) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            Order orderObj = orderModel.getOrderById("order", id);
+            orderObj.setStatus("Da nop");
+            orderModel.update(orderObj, "order");
+        } else if ("cancel".equals(acction)) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            Order orderObj = orderModel.getOrderById("order", id);
+            orderObj.setStatus("Da huy");
+            orderModel.update(orderObj, "order");
+        } 
         
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("trangQuanTri.jsp");
         
@@ -25,11 +70,26 @@ public class AdminDichVuServlet extends HttpServlet {
             return;
         }
         
-        AdminDichVuModel adminDichVuModel = new AdminDichVuModel();
-        List<DichVu> listDichVu = adminDichVuModel.getAllDichVu();
+        
+        List<Order> listOrder = orderModel.getListOrderByMien("order", mien);
+        request.setAttribute("listOrder", listOrder);
+        List<DichVu> listDichVu;
+        if (mien.equals("admin")) {
+            listDichVu = adminDichVuModel.getAllDichVu("dich_vu");
+        } else {
+            listDichVu = adminDichVuModel.getListDichVuByVung("dich_vu", mien);
+        }
         request.setAttribute("listDichVu", listDichVu);
         request.setAttribute("request", "cacGoiDichVu");
         requestDispatcher.forward(request, response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doProcess(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doProcess(request, response);
     }
 
 }
